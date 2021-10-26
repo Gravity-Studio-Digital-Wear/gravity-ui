49bd5cf439b4b519f17271a0d6c6ce9ed8b4bb4c
Timofey Semenyuk <rustfy@gmail.com>
2021-10-26 23:08:57 +0300

    fixed slider

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,11 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import './fonts/stylesheet.css'
 
-// import 'swiper/react/';
-// import "swiper/css/pagination";
+import 'swiper/swiper-bundle.min.css'
+import 'swiper/swiper.min.css'
+
+import "swiper/components/navigation/navigation.min.css";
+import "swiper/components/pagination/pagination.min.css";
 
 import App from "./App";
 
