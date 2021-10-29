f19948d02c69eca1c352064100dfc085dfcc668e
dvasin <dvasin@wavesplatform.com>
2021-10-29 21:55:19 +0300

    Fixed E_CHECKOUT_SUCCESS

--- src/utils/amplitude.ts
+++ src/utils/amplitude.ts
@@ -1,6 +1,8 @@
 import amplitude from 'amplitude-js';
 import { getEnv } from './env'
 
+let initialized = false
+
 export const initAmplitude = () => {
   const API_KEY = getEnv('REACT_AMPLITUDE_API_KEY', '4a58a25058aed5b2af7b14c092ff5de5')
   amplitude.getInstance().init(API_KEY, null, {
@@ -11,21 +13,34 @@ export const initAmplitude = () => {
   if (amplitude.getInstance().isNewSession()) {
     sendAmplitudeData('E_NEW_SESSION')
   }
+  initialized = true
 };
 
 export const setAmplitudeUserDevice = installationToken => {
+  if (!initialized) {
+    initAmplitude()
+  }
   amplitude.getInstance().setDeviceId(installationToken);
 };
 
 export const setAmplitudeUserId = userId => {
+  if (!initialized) {
+    initAmplitude()
+  }
   amplitude.getInstance().setUserId(userId);
 };
 
 export const setAmplitudeUserProperties = properties => {
+  if (!initialized) {
+    initAmplitude()
+  }
   amplitude.getInstance().setUserProperties(properties);
 };
 
 
 export const sendAmplitudeData = (eventType: string, eventProperties?: any) => {
+  if (!initialized) {
+    initAmplitude()
+  }
   amplitude.getInstance().logEvent(eventType, eventProperties);
 };
