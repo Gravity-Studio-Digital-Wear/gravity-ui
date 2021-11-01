6fdc6bab368d1732c480d1ed3598060d36600c30
Timofey Semenyuk <rustfy@gmail.com>
2021-11-01 10:32:42 +0300

    fixed matic prod mainnet

--- src/services/Web3.ts
+++ src/services/Web3.ts
@@ -2,14 +2,19 @@ import {Magic} from "magic-sdk";
 import {OAuthExtension} from "@magic-ext/oauth";
 import Web3 from "web3";
 
-const customNodeOptions = { // Test network
+const testNetwork = { // Test network
     rpcUrl: 'https://rpc-mumbai.matic.today',
     chainId: 80001,
 };
 
+const mainNetwork = {
+    rpcUrl: 'https://rpc-mainnet.maticvigil.com/', // Polygon RPC URL
+    chainId: 137, // Polygon chain id
+}
+
 export const magic = new Magic("pk_live_2F5A5F02878460F4", {
     extensions: [new OAuthExtension()],
-    network: customNodeOptions
+    network: process.env.NODE_ENV === 'production' ? mainNetwork : testNetwork
 });
 
 
